3f90b4bda89a3e22b0b2e58a24caacb498a768ea
hunter <hunterkeng@hotmail.com>
2019-03-09 10:03:14 +1300

    test git

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class Clock extends React.Component {
+class Table extends React.Component {
     constructor(){
         super();
         this.state = {date: new Date()};
